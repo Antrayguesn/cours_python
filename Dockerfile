394e8b84dce7d0346071edeb44b6793141d34e3d
Nicolas ANTRAYGUES <nicolas.antraygues@aigyre.fr>
2023-12-06 22:15:04 +0100

    Fin T

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM nginx
 
-COPY *.html /usr/share/nginx/html/
+COPY build/*.html /usr/share/nginx/html/
 COPY images  /usr/share/nginx/html/images/
 COPY TP/*.py /usr/share/nginx/html/codes/
--- generate.sh
+++ generate.sh
@@ -2,3 +2,7 @@ export PLANTUML_URL="https://uml.aigyre.fr/"
 bundle exec asciidoctor-revealjs -r asciidoctor-plantuml Presentation/presentation.adoc -o build/presentation.html
 bundle exec asciidoctor -r asciidoctor-plantuml Presentation/presentation.adoc -o build/cours.html
 bundle exec asciidoctor -r asciidoctor-plantuml TP/LaMissionDeCesar.adoc -o build/TP.html
+
+docker build . -t registry.aigyre.fr/tp:latest
+
+docker push registry.aigyre.fr/tp:latest
